1a8cc763ed74f9be16ae444d4664e4deae01f955
Damiensimon-dev <85479424+Damiensimon-dev@users.noreply.github.com>
2022-01-07 11:54:26 +0100

    juste une nouvelle solution jeu sur l'ide)

---
+++ visualStudio/jeu/jeu/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace jeu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                int numberOfTemperature = int.Parse(Console.ReadLine()); // the number of temperatures to analyse
+                string[] listTemperature = Console.ReadLine().Split(' ');
+                for (int indexTemperatures = 0; indexTemperatures < numberOfTemperature; indexTemperatures++)
+                {
+                    int t = int.Parse(listTemperature[indexTemperatures]);// a temperature expressed as an integer ranging from -273 to 5526
+                }
+
+                // Write an answer using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+
+                Console.WriteLine("result");
+            
+        }
+    }
+}
