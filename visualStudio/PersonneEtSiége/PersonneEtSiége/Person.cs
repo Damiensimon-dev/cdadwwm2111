a798274fb9cefa398bd1d733baf189f71f57d1a3
Damiensimon-dev <85479424+Damiensimon-dev@users.noreply.github.com>
2022-01-14 11:46:38 +0100

    ajout de l'exercice personne et siége

---
+++ visualStudio/PersonneEtSiége/PersonneEtSiége/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonneEtSiége
+{
+    class Person
+    {
+        string _lastName;
+        string _firstName;
+        int _age;
+        bool _isStandingUp;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            private set { _lastName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            private set { _firstName = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            private set { _age = value; }
+        }
+
+        public bool IsStandingUp
+        {
+            get { return _isStandingUp; }
+            private set { _isStandingUp = value; }
+        }
+        
+
+
+    }
+}
---
+++ visualStudio/PersonneEtSiége/PersonneEtSiége/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonneEtSiége
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
