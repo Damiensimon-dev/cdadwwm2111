73d2adfa72a4d73a3ed7e6ce1b2c94f373faa514
Damiensimon-dev <85479424+Damiensimon-dev@users.noreply.github.com>
2022-01-19 16:52:16 +0100

    ajout exercice montre

---
+++ visualStudio/Montre/Montre/Montre.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montre
+{
+    class Montre
+    {
+        private int _heure;
+        private int _minute;
+
+        public Montre(int heure, int minute)
+        {
+
+        }
+
+        public Montre(Montre montre)
+        {
+
+        }
+
+
+    }
+}
